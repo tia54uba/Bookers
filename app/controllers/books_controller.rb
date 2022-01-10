cdb38752196748652c2d2f420c843cd8c4127db6
EC2 Default User <ec2-user@ip-172-31-35-28.ec2.internal>
2022-01-10 13:18:23 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+
+
     private
   # ストロングパラメータ
   def book_params
